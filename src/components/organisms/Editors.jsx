63aa1782b604ed39b83da447cb1dc36c860feb98
fishnal <vpatel14@terpmail.umd.edu>
2019-12-08 01:55:36 -0500

    DRY-ed up editors section

--- src/components/organisms/Editors.jsx
+++ src/components/organisms/Editors.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 
+import editors from '../../data/editors.json';
+
 export const Editors = () => (
   <div className='editors'>
     <span>Editors</span>
 
     <dl>
       <dt><span>Project Editor</span></dt>
-      <dd>Christine Condon</dd>
+      <dd>{editors.projectEditor}</dd>
     </dl>
 
     <dl>
       <dt><span>Copy Editors</span></dt>
-      <dd>Daisy Grant, Arya Hodjat, Ryan Romano, Leah Brennan</dd>
+      <dd>
+        {
+          editors.copyEditors.map((copyEditor, i) => {
+            if (i < editors.copyEditors.length - 1) {
+              return `${copyEditor}, `;
+            } else {
+              return copyEditor;
+            }
+          })
+        }
+      </dd>
     </dl>
   </div>
 );
